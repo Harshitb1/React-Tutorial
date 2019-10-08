4c496b89bcb6cd2eeb8cf05b0ddc6e8f8f255821
Harshit Batra <harshitbatra56@yahoo.in>
2019-10-09 02:48:39 +0530

    add todo feature added

--- src/App.js
+++ src/App.js
@@ -2,42 +2,58 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// class App extends React.Component {
-//   state =  {
-//     allTodos:[
-//       {name:  "Hi",id: "1"},
-//       {name:"Yo", id: "2"}
-//     ]
-//   }
-//   console.log(this.state.allTodos)
-//   return (
-
-//     <div className="App">
-//       {/* {this.state.allTodos} */}
-//     </div>
-
-//   );
-// }
-var todos=[];
-//  renderTodos=()=>{
-   
-//  }
-
+// class based component
 class App extends React.Component {
   state =  {
-        allTodos:[
-          {name:"Hi",id: "1"},
-          {name:"Yo", id: "2"}
-        ]
-      }
+    allTodos:[
+      {name:"Hi",id: "1"},
+      {name:"Yo", id: "2"}
+    ],
+    newTodo:""
+  }
+
+  renderTodos=()=>{
+    return this.state.allTodos.map(todo=>(
+      <div> {todo.id}. {todo.name}</div>
+    ))
+  }
+
+  onChange=(e)=>{
+    this.setState({[e.target.name]:e.target.value})
+  }
+
+  onSubmit= (e)=>{
+    e.preventDefault();
+    this.setState({allTodos:[...this.state.allTodos,{name:this.state.newTodo,id:this.state.allTodos.length+1}]})    
+    this.setState({ newTodo:""})
+
+  }
      
   render() {
     
-    return this.state.allTodos.map(todo=>(
-      <div> {todo.id}. {todo.name}</div>
-      ));  
-   
- 
+    return (
+      <div>
+
+        {this.renderTodos()}
+        
+        <form onSubmit={this.onSubmit}>
+        
+          <input 
+          name="newTodo"
+          placeholder="meet samanyu" 
+          value = {this.state.newTodo}
+          onChange={this.onChange}
+          />
+
+          <input
+          type="submit"
+          value="add ToDo"
+          />
+
+        </form>
+
+      </div>
+      );   
   }
 
 }
